800feff150dc5cb7eaa30fc470791388702dab78
Sami Sipari <sami.sipari@tuni.fi>
2020-08-06 09:59:25 +0300

    Context added

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,14 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect, Fragment, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import GithubContext from "../../context/github/githubContext";
 
-const User = ({ user, loading, repos, getUser, getUserRepos, match }) => {
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { loading, user, getUser, getUserRepos } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
@@ -92,17 +96,9 @@ const User = ({ user, loading, repos, getUser, getUserRepos, match }) => {
         <div className="badge badge-light">Public Repos: {public_repos}</div>
         <div className="badge badge-dark">Public Gists: {public_gists}</div>
       </div>
-      <Repos repos={repos} />
+      <Repos />
     </Fragment>
   );
-};
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
-  repos: PropTypes.array.isRequired,
 };
 
 export default User;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import UserItem from "./UserItem";
 import Spinner from "../layout/Spinner";
-import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
 
-const Users = ({ users, loading }) => {
-  return loading ? (
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+
+  return githubContext.loading ? (
     <Spinner />
   ) : (
     <div style={userStyle}>
-      {users.map((user) => (
+      {githubContext.users.map((user) => (
         <UserItem key={user.id} user={user} />
       ))}
     </div>
@@ -19,11 +21,6 @@ const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
   gridGap: "1rem",
-};
-
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired,
 };
 
 export default Users;
